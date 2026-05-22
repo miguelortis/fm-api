2085b5d7f1eb25f5bdd47f0bc29988274e600766
p-arguelles <pedroarguelles1996@gmail.com>
2026-05-21 21:18:11 -0400

    Reorganizar el proceso de arranque del servidor y agregar manejo de errores

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,34 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  try {
+    console.log('--- INICIANDO BOOTSTRAP DE UNEFM SALUD ---');
+    console.log('Verificando Puerto:', process.env.PORT);
+    console.log('Verificando URI de Mongo existe:', !!process.env.MONGO_URI);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // Elimina propiedades que no estén en el DTO
-      forbidNonWhitelisted: true, // Lanza error si envían propiedades de más
-      transform: true, // Convierte los tipos automáticamente
-    }),
-  );
-  app.setGlobalPrefix('api');
+    const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost:3000', '*'], // o un array de orígenes permitidos
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
-  console.log(`Server is running on port ${port}`);
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // Elimina propiedades que no estén en el DTO
+        forbidNonWhitelisted: true, // Lanza error si envían propiedades de más
+        transform: true, // Convierte los tipos automáticamente
+      }),
+    );
+    app.setGlobalPrefix('api');
+
+    app.enableCors({
+      origin: ['http://localhost:3000', '*'], // o un array de orígenes permitidos
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
+    const port = process.env.PORT ?? 3000;
+    await app.listen(port);
+    console.log(`Server is running on port ${port}`);
+  } catch (error) {
+    console.error('!!! ERROR CRÍTICO EN EL ARRANQUE DEL SERVIDOR !!!');
+    console.error(error); // <-- Esto nos dirá la verdad en el log de Render
+    process.exit(1);
+  }
 }
 void bootstrap();
